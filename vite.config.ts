97cd5d8ceeb5ce0ae1bba39ccb78ecfadda360d6
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2024-03-09 03:09:26 -0800

    setup vite for library mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,51 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import { extname, relative, resolve } from "path";
+import { fileURLToPath } from "node:url";
+import { glob } from "glob";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    libInjectCss(),
+    dts({ include: ["lib"], exclude: ["**/*.test.tsx", "**/*.test.ts"] }),
+  ],
+  build: {
+    copyPublicDir: false,
+    lib: {
+      entry: resolve(__dirname, "lib/main.ts"),
+      formats: ["es"],
+    },
+    rollupOptions: {
+      // files to ignore
+      external: ["react", "react/jsx-runtime"],
+      input: Object.fromEntries(
+        glob
+          .sync("lib/**/*.{ts,tsx}")
+          .filter((file) => {
+            return !/\.test\.ts/.test(file) && file !== "lib/vite-env.d.ts";
+          })
+          .map((file) => [
+            // The name of the entry point
+            // lib/nested/foo.ts becomes nested/foo
+            relative("lib", file.slice(0, file.length - extname(file).length)),
+            // The absolute path to the entry file
+            // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
+            fileURLToPath(new URL(file, import.meta.url)),
+          ]),
+      ),
+      output: {
+        assetFileNames: "assets/[name][extname]",
+        entryFileNames: "[name].js",
+      },
+    },
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+  },
+});
