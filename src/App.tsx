57413ce95f737d60d2a5cb4cd8fe431a8a681d5d
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2024-03-13 09:22:47 -0700

    add pdf viewer to homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
 import Viewer from "@samvera/clover-iiif/viewer";
+import PdfViewer from "../lib/PdfViewer/components/PdfViewer";
 
 function App() {
   return (
-    <Viewer iiifContent="https://api.dc.library.northwestern.edu/api/v2/works/8a833741-74a8-40dc-bd1d-c416a3b1bb38?as=iiif" />
+    <Viewer
+      iiifContent="http://localhost:3000/api/pdf"
+      customDisplays={[
+        {
+          display: {
+            component: PdfViewer,
+            componentProps: {
+              showPdfThumbnails: true,
+              showPdfToolBar: true,
+              showPdfSearch: true,
+              showPdfZoom: true,
+              showPdfPaging: true,
+              showPdfRotate: true,
+              showPdfTwoPageSpread: true,
+              showPdfFullScreen: true,
+              canvasHeight: "71.8vh",
+            },
+          },
+          target: {
+            paintingFormat: ["application/pdf"],
+          },
+        },
+      ]}
+      options={{ canvasHeight: "71.8vh" }}
+    />
   );
 }
 
